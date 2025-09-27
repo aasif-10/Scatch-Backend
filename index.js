4b68d66c8d8d76af6ffe1543ab9fbc1cf36b0fc9
Aasif Khan <aasifkhan.a2006@gmail.com>
2025-09-27 21:09:04 +0530

    Inital commit- project setup

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+const cookieParser = require("cookie-parser");
+const path = require("path");
+const userModel = require("./models/user-model");
+const productModel = require("./models/product-model");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.listen(3000);
