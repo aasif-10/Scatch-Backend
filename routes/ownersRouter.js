a9bf56d936a9df491c8f54453781c4a9277c7ddf
Aasif Khan <aasifkhan.a2006@gmail.com>
2025-10-24 15:59:13 +0530

    Oct 24th - Built Create Product page

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -25,4 +25,12 @@ router.get("/", function (req, res) {
   res.send("its working!");
 });
 
+router.get("/admin", (req, res) => {
+  res.render("createProducts");
+});
+
+router.post("/admin/create", (req, res) => {
+  res.send("received");
+});
+
 module.exports = router;
